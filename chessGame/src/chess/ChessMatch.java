6cdf9cfe1b8badfd616bbd68fd8c8a825fc60aa5
Gabriel Vitor <gabrielvitor0309@gmail.com>
2023-07-10 17:33:26 -0300

    Possiveis movimentos das peças

--- chessGame/src/chess/ChessMatch.java
+++ chessGame/src/chess/ChessMatch.java
@@ -37,6 +37,9 @@ public class ChessMatch {
 		if (!board.thereIsAPiece(postion)) {
 			throw new ChessException("There is no piece on source position");
 		}
+		if (!board.piece(postion).isThereAnyPossibleMove()) {
+			throw new ChessException("There is no possible moves for the chosen piece");
+		}
 	}
 	
 	private Piece makeMove(Position source, Position target) {
